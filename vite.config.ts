482c8005fb1c7e7be2873520029b8777c8199df8
Newkiller <robocraft.tier5@outlook.de>
2022-12-31 08:10:07 +0100

    fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,41 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePWA({ registerType: 'autoUpdate' })],
+  build: {
+    outDir: './docs',
+  },
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // includeAssets: ['favicon.svg'],
+      // workbox: {
+      //   globPatterns: ['**/*.{js,css,html,svg,png,woff2}'],
+      // },
+      manifest: {
+        background_color: '#000000',
+        theme_color: '#7E1F86',
+        name: 'TaskManager1.0',
+        short_name: 'TaskManager1.0',
+        start_url: '/TaskManager/',
+        display: 'standalone',
+        // icons: [
+        //   {
+        //     src: '![cardBackside](public/img/cardBackside.png)',
+        //     sizes: '144x144',
+        //     type: 'image/png',
+        //     purpose: 'any',
+        //   },
+        // ],
+      },
+    }),
+  ],
+  server: {
+    port: 8080,
+    host: true,
+    hmr: {
+      host: 'localhost',
+    },
+  },
+  base: './',
 });
