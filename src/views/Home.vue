7027123fe927d96252dde1705787925675c5e154
Newkiller <robocraft.tier5@outlook.de>
2023-01-01 19:14:12 +0100

    add descriptions to home menu

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,23 @@
 <template>
   <div class="d-flex flex-column justify-content-around align-items-center w-100 py-5" style="height: 100vh">
-    <control @click.stop="router.push('/friends')"><i class="far fa-user"></i></control>
-    <control @click.stop="router.push('/lists')"><i class="far fa-list-alt"></i></control>
-    <control @click.stop="router.push('/settings')"><i class="fas fa-cogs"></i></control>
+    <control @click.stop="router.push('/friends')">
+      <div class="d-flex flex-column justify-content-center align-items-center">
+        <i class="far fa-user"></i>
+        <div class="mt-2" style="font-size: 15px">Friends</div>
+      </div>
+    </control>
+    <control @click.stop="router.push('/lists')">
+      <div class="d-flex flex-column justify-content-center align-items-center">
+        <i class="far fa-list-alt"></i>
+        <div class="mt-2" style="font-size: 15px">Lists</div>
+      </div>
+    </control>
+    <control @click.stop="router.push('/settings')">
+      <div class="d-flex flex-column justify-content-center align-items-center">
+        <i class="fas fa-cogs"></i>
+        <div class="mt-2" style="font-size: 15px">Settings</div>
+      </div>
+    </control>
   </div>
 </template>
 <script setup lang="ts">
